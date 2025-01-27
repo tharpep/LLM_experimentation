76f2b598a89eace3e8f285e304a19f1f29c80159
Pryce Tharpe <pjtharpe04@gmail.com>
2025-01-27 18:28:51 -0500

    Added parameters to output for local runnning

--- LocalLLM.py
+++ LocalLLM.py
@@ -2,6 +2,14 @@ from openai import OpenAI
 
 import re
 
+
+"""
+For terminal usage, run ollama run deepseek-r1:1.b
+
+For or API usage, run ollama serve
+"""
+
+
 client = OpenAI(api_key="not-needed", base_url="http://localhost:11434/v1")
 
 stay = True
