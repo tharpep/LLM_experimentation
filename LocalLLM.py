f48cc95357e542e95323583d4e36cdc156e17755
Pryce Tharpe <pjtharpe04@gmail.com>
2025-01-27 18:14:06 -0500

    Created working localLLM

---
+++ LocalLLM.py
@@ -0,0 +1,29 @@
+from openai import OpenAI
+
+import re
+
+client = OpenAI(api_key="not-needed", base_url="http://localhost:11434/v1")
+
+stay = True
+
+while stay:
+    inp = input("\nPlease input query(Type /leave to leave): ")
+
+    if inp == "/leave":
+        stay = False
+    else:
+        response = client.chat.completions.create(
+            model="deepseek-r1:1.5b",
+            messages=[
+                {"role": "system", "content": "You are a helpful AI assistant."},
+                {"role": "user", "content": inp},
+            ],
+            stream=False
+        )
+
+        full_response =  response.choices[0].message.content
+        match = re.search(r'</think>(.*)', full_response, re.DOTALL)
+        out = match.group(1).strip() if match else full_response
+
+        print(out)
+        
